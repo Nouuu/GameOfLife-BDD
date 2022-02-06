94bdfd80ff4093414110a2bdef709e3235d5e88e
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-02-06 15:44:54 +0100

    better display

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { Board } from './game/Board';
+import { black, bgGreen } from 'colors';
 
 const board = Board.initRandom(15, 30);
 
 setInterval(() => {
     console.clear();
-    console.log(board.toString());
+    console.log(board.toString().replace(/ x /gm, black(bgGreen(' x '))));
     board.nextStep();
-}, 1000);
+}, 250);
